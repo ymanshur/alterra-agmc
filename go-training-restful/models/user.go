f8650c1ca0996ddc5e67c1547a6b6c7f5187170d
Yusuf Manshur <ymanshur@gmail.com>
2022-09-15 13:08:52 +0700

    Modified User struct (not using gorm.Model)

--- go-training-restful/models/user.go
+++ go-training-restful/models/user.go
@@ -1,12 +1,16 @@
 package models
 
 import (
-	"github.com/jinzhu/gorm"
+	"time"
 )
 
 type User struct {
-	gorm.Model
-	Name     string `json:"name" form:"name"`
-	Email    string `json:"email" form:"email"`
-	Password string `json:"password" form:"password"`
+	// gorm.Model
+	ID        uint       `json:"id" gorm:"primary_key"`
+	CreatedAt time.Time  `json:"created_at"`
+	UpdatedAt time.Time  `json:"updated_at"`
+	DeletedAt *time.Time `json:"deleted_at" sql:"index"`
+	Name      string     `json:"name" form:"name"`
+	Email     string     `json:"email" form:"email"`
+	Password  string     `json:"password" form:"password"`
 }
