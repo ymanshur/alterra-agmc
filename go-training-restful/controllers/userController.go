903d758535efc38fd414ba85a1f68f5e483dffa8
Yusuf Manshur <ymanshur@gmail.com>
2022-09-15 13:10:16 +0700

    Added POST and GET detail controllers

--- go-training-restful/controllers/userController.go
+++ go-training-restful/controllers/userController.go
@@ -7,14 +7,41 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-func GetUserControllers(c echo.Context) error {
-	users, e := database.GetUsers()
+func GetUsersController(c echo.Context) error {
+	users, err := database.GetUsers()
 
-	if e != nil {
-		return echo.NewHTTPError(http.StatusBadRequest, e.Error())
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
 	}
 	return c.JSON(http.StatusOK, map[string]interface{}{
-		"status": "success",
-		"users":  users,
+		"status":  "success",
+		"message": "success get all users",
+		"data":    users,
+	})
+}
+
+func CreateUserController(c echo.Context) error {
+	user, err := database.CreateUser(c)
+
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+	return c.JSON(http.StatusCreated, map[string]interface{}{
+		"status":  "success",
+		"message": "success create new user",
+		"data":    user,
+	})
+}
+
+func GetUserController(c echo.Context) error {
+	user, err := database.GetUser(c)
+
+	if err != nil {
+		return echo.NewHTTPError(http.StatusNotFound, err.Error())
+	}
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"status":  "success",
+		"message": "success get a user",
+		"data":    user,
 	})
 }
