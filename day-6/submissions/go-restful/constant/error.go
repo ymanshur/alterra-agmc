385c457ccbe04f9280724ad643531cd7b36b20d1
Yusuf Manshur <ymanshur@gmail.com>
2022-09-21 13:08:13 +0700

    Revert "Added intial day-6 project submission"
    This reverts commit 9910af7ebba8a0ff8135e59f9f1e43cabae6f23b.

--- day-6/submissions/go-restful/constant/error.go
+++
@@ -1,9 +0,0 @@
-package constant
-
-import "errors"
-
-var (
-	ErrInvalidUrlParam = errors.New("invalid id url parameter")
-	ErrInvalidInput    = errors.New("invalid input")
-	ErrRecordNotFound  = errors.New("record not found")
-)
--- day-6/submissions/go-restful/model/book.go
+++
@@ -1,9 +0,0 @@
-package model
-
-type (
-	Book struct {
-		ID     int    `json:"id"`
-		Title  string `json:"title" form:"title"`
-		Author string `json:"author" form:"author"`
-	}
-)
