1e294d20833a5e74953c251bf370d84446bf60b0
Yusuf Manshur <ymanshur@gmail.com>
2022-09-23 21:56:30 +0700

    Removed CURRENT_TIMESTAMP default value, mysql image not support

--- day-6/submissions/go-restful/internal/model/user.go
+++ day-6/submissions/go-restful/internal/model/user.go
@@ -6,8 +6,8 @@ import (
 
 type User struct {
 	ID        uint      `gorm:"primaryKey" json:"id"`
-	CreatedAt time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"created_at"`
-	UpdatedAt time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"updated_at"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 	Name      string    `gorm:"size:256" json:"name" validate:"required"`
 	Email     string    `gorm:"not null;unique" json:"email" validate:"required,email"`
 	Password  string    `gorm:"not null" json:"password" validate:"required"`
